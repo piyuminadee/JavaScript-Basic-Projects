fbec5fba195df3a88064cbed653f3e9fdecef004
piyuminadee <nadeesh-cs20024@stu.kln.ac.lk>
2024-04-14 15:06:12 +0530

    generate html

---
+++ script/aazon.js
@@ -0,0 +1,30 @@
+const product = [{
+    image : "images/ratings/rating-45.png",
+     name : "Black and Gray Athletic Cotton Socks - 6 Pairs",
+     rating : {
+        stars:4.5,
+        count : 87
+     
+    },
+    priceCents : 1090
+
+},{
+    image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+     name : "Intermediate Size Basketball",
+     rating : {
+        stars:4.0,
+        count : 127
+     
+    },
+    priceCents : 2095
+},
+{
+    image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name : "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating : {
+       stars:4.5,
+       count : 56
+    
+   },
+   priceCents : 799
+}]
